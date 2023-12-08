b8167bec7d3da74587aca7afb19ed05a296995f1
Dan Wilder <12650041+WilderDan@users.noreply.github.com>
2023-12-07 18:57:58 -0600

    Add subtract function

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -1,5 +1,6 @@
 const calc = require("./calculator.js");
 
+// Add
 test("adds 1 + 2", () => {
   expect(calc.add(1, 2)).toBe(3);
 });
@@ -10,4 +11,17 @@ test("adds negative numbers", () => {
 
 test("adds larger numbers", () => {
   expect(calc.add(3000, 6999)).toBe(9999);
+});
+
+// Subtract
+test("Subtracts 8 - 6", () => {
+  expect(calc.subtract(8, 6)).toBe(2);
+});
+
+test("Subtracts negative numbers", () => {
+  expect(calc.subtract(80, -14)).toBe(94);
+});
+
+test("Subtracts when 2nd operand greater than first", () => {
+  expect(calc.subtract(10, 20)).toBe(-10);
 });
